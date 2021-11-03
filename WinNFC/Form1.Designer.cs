3615214ffd05493e3df3e60448f6cec7511a2b7d
junior202 <junior202@qq.com>
2021-11-03 21:06:16 +0800

    添加项目文件。

---
+++ WinNFC/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace WinNFC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Bt_NFC = new System.Windows.Forms.Button();
+            this.NFC_Rate = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NFC_Port = new System.Windows.Forms.ComboBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(633, 184);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "显示区";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.LawnGreen;
+            this.textBox1.Location = new System.Drawing.Point(13, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(610, 147);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Bt_NFC);
+            this.groupBox2.Controls.Add(this.NFC_Rate);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.NFC_Port);
+            this.groupBox2.Location = new System.Drawing.Point(12, 212);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 323);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "NFC接口";
+            // 
+            // Bt_NFC
+            // 
+            this.Bt_NFC.Location = new System.Drawing.Point(111, 288);
+            this.Bt_NFC.Name = "Bt_NFC";
+            this.Bt_NFC.Size = new System.Drawing.Size(75, 23);
+            this.Bt_NFC.TabIndex = 4;
+            this.Bt_NFC.Text = "连接设备";
+            this.Bt_NFC.UseVisualStyleBackColor = true;
+            this.Bt_NFC.Click += new System.EventHandler(this.Bt_NFC_Click);
+            // 
+            // NFC_Rate
+            // 
+            this.NFC_Rate.FormattingEnabled = true;
+            this.NFC_Rate.Items.AddRange(new object[] {
+            "115200",
+            "76800",
+            "56700",
+            "19200",
+            "9600"});
+            this.NFC_Rate.Location = new System.Drawing.Point(65, 51);
+            this.NFC_Rate.Name = "NFC_Rate";
+            this.NFC_Rate.Size = new System.Drawing.Size(121, 20);
+            this.NFC_Rate.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "波特率：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "端口号：";
+            // 
+            // NFC_Port
+            // 
+            this.NFC_Port.FormattingEnabled = true;
+            this.NFC_Port.Location = new System.Drawing.Point(65, 20);
+            this.NFC_Port.Name = "NFC_Port";
+            this.NFC_Port.Size = new System.Drawing.Size(121, 20);
+            this.NFC_Port.TabIndex = 1;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.listBox2);
+            this.groupBox4.Location = new System.Drawing.Point(218, 212);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(427, 323);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "蓝牙设备";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(6, 20);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(411, 292);
+            this.listBox2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(347, 47);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "写入NFC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 488);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(382, 29);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.button1);
+            this.groupBox5.Controls.Add(this.textBox4);
+            this.groupBox5.Controls.Add(this.label7);
+            this.groupBox5.Controls.Add(this.progressBar1);
+            this.groupBox5.Controls.Add(this.label5);
+            this.groupBox5.Controls.Add(this.textBox3);
+            this.groupBox5.Controls.Add(this.textBox2);
+            this.groupBox5.Controls.Add(this.label6);
+            this.groupBox5.Location = new System.Drawing.Point(651, 12);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(394, 523);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "操作区";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(28, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "设备MAC :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(107, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(254, 29);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(28, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "产品类码 :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox3.Location = new System.Drawing.Point(107, 87);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(254, 29);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(28, 145);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 17);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "鸿蒙码流 :";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox4.Location = new System.Drawing.Point(107, 138);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(254, 29);
+            this.textBox4.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 547);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox5);
+            this.Name = "Form1";
+            this.Text = "NFC量产工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox NFC_Port;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox NFC_Rate;
+        private System.Windows.Forms.Button Bt_NFC;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WinNFC/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+
+namespace WinNFC
+{
+    public partial class Form1 : Form
+    {
+        String serialPortName;
+
+
+        /// <summary>
+        /// 多线程的创建和管理
+        /// </summary>
+        /// 
+
+       // Dictionary<string, Thread> dict = new Dictionary<string, Thread>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 搜索蓝牙
+        /// </summary>
+        /// <returns></returns>
+        public static Dictionary<string, BluetoothAddress> GetClientBluetooth()
+        {
+            BluetoothRadio radio = BluetoothRadio.PrimaryRadio;//获取蓝牙适配器
+            
+                Dictionary<string, BluetoothAddress> dicBluetooth = new Dictionary<string, BluetoothAddress>();
+                BluetoothClient client = new BluetoothClient();
+                BluetoothDeviceInfo[] devices = client.DiscoverDevices();//搜索蓝牙 10秒钟
+                foreach (BluetoothDeviceInfo d in devices)
+                {
+                    dicBluetooth[d.DeviceName] = d.DeviceAddress;
+                }
+                return dicBluetooth;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0219)
+            {//设备改变
+                if (m.WParam.ToInt32() == 0x8004)
+                {//usb串口拔出
+                    string[] ports = System.IO.Ports.SerialPort.GetPortNames();//重新获取串口
+                    NFC_Port.Items.Clear();//清除comboBox里面的数据
+                    NFC_Port.Items.AddRange(ports);//给comboBox1添加数据
+                    if (Bt_NFC.Text == "关闭设备")
+                    {//用户打开过串口
+                        if (!serialPort1.IsOpen)
+                        {//用户打开的串口被关闭:说明热插拔是用户打开的串口
+                            Bt_NFC.Text = "连接设备";
+                            serialPort1.Dispose();//释放掉原先的串口资源
+                            NFC_Port.SelectedIndex = NFC_Port.Items.Count > 0 ? 0 : -1;//显示获取的第一个串口号
+                        }
+                        else
+                        {
+                            NFC_Port.Text = serialPortName;//显示用户打开的那个串口号
+                        }
+                    }
+                    else
+                    {//用户没有打开过串口
+                        NFC_Port.SelectedIndex = NFC_Port.Items.Count > 0 ? 0 : -1;//显示获取的第一个串口号
+                    }
+                }
+                else if (m.WParam.ToInt32() == 0x8000)
+                {//usb串口连接上
+                    string[] ports = System.IO.Ports.SerialPort.GetPortNames();//重新获取串口
+                    NFC_Port.Items.Clear();
+                    NFC_Port.Items.AddRange(ports);
+                    if (Bt_NFC.Text == "关闭设备")
+                    {//用户打开过一个串口
+                        NFC_Port.Text = serialPortName;//显示用户打开的那个串口号
+                    }
+                    else
+                    {
+                        NFC_Port.SelectedIndex = NFC_Port.Items.Count > 0 ? 0 : -1;//显示获取的第一个串口号
+                    }
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            NFC_Port.Items.AddRange(ports);
+            //Com_Port.Items.AddRange(ports);
+            NFC_Port.SelectedIndex = NFC_Port.Items.Count > 0 ? 0 : -1;
+            //Com_Port.SelectedIndex = Com_Port.Items.Count > 0 ? 0 : -1;
+
+            
+
+            NFC_Rate.Text = "115200";
+            //Com_Rate.Text = "115200";
+            BluetoothRadio radio = BluetoothRadio.PrimaryRadio;//获取蓝牙适配器
+            if (radio == null)
+            {
+                Console.WriteLine("没有找到本机蓝牙设备!");
+            }
+            else
+            {
+
+                listBox2.DataSource = new BindingSource(GetClientBluetooth(), null);
+            }
+        }
+
+        private void Bt_NFC_Click(object sender, EventArgs e)
+        {
+            if (Bt_NFC.Text == "连接设备")
+            {//如果按钮显示的是打开串口
+                try
+                {//防止意外错误
+                    serialPort1.PortName = NFC_Port.Text;//获取comboBox1要打开的串口号
+                    serialPort1.BaudRate = int.Parse(NFC_Rate.Text);//获取comboBox2选择的波特率
+                    serialPort1.DataBits = int.Parse("8");//设置数据位
+                    /*设置停止位*/
+                    serialPort1.StopBits = System.IO.Ports.StopBits.One; 
+                    /*设置奇偶校验*/
+                   serialPort1.Parity = System.IO.Ports.Parity.None;
+
+                    serialPort1.Open();//打开串口
+
+                    serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived);
+                    Bt_NFC.Text = "关闭设备";//按钮显示关闭串口
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show("打开失败" + err.ToString(), "提示!");//对话框显示打开失败
+                }
+            }
+            else
+            {//要关闭串口
+                try
+                {//防止意外错误
+                    serialPort1.Close();//关闭串口
+                }
+                catch (Exception) { }
+                button1.Text = "连接设备";//按钮显示打开
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            int len = serialPort1.BytesToRead;//获取可以读取的字节数
+            byte[] buff = new byte[len];//创建缓存数据数组
+            serialPort1.Read(buff, 0, len);//把数据读取到buff数组
+            string str = Encoding.Default.GetString(buff);//Byte值根据ASCII码表转为 String
+            Invoke((new Action(() => //C# 3.0以后代替委托的新方法
+            {
+                textBox1.AppendText(str);//对话框追加显示数据
+            })));
+        }
+
+        /// <字节数组转16进制字符串>
+        /// <param name="bytes"></param>
+        /// <returns> String 16进制显示形式</returns>
+        public static string byteToHexStr(byte[] bytes)
+        {
+            string returnStr = "";
+            try
+            {
+                if (bytes != null)
+                {
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        returnStr += bytes[i].ToString("X2");
+                        returnStr += " ";//两个16进制用空格隔开,方便看数据
+                    }
+                }
+                return returnStr;
+            }
+            catch (Exception)
+            {
+                return returnStr;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("OK");//串口发送数据
+        }
+
+        /// <字符串转16进制格式,不够自动前面补零>
+        /// 
+        /// </summary>
+        /// <param name="hexString"></param>
+        /// <returns></returns>
+        private static byte[] strToToHexByte(String hexString)
+        {
+            int i;
+            hexString = hexString.Replace(" ", "");//清除空格
+            if ((hexString.Length % 2) != 0)//奇数个
+            {
+                byte[] returnBytes = new byte[(hexString.Length + 1) / 2];
+                try
+                {
+                    for (i = 0; i < (hexString.Length - 1) / 2; i++)
+                    {
+                        returnBytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+                    }
+                    returnBytes[returnBytes.Length - 1] = Convert.ToByte(hexString.Substring(hexString.Length - 1, 1).PadLeft(2, '0'), 16);
+                }
+                catch
+                {
+                    MessageBox.Show("含有非16进制字符", "提示");
+                    return null;
+                }
+                return returnBytes;
+            }
+            else
+            {
+                byte[] returnBytes = new byte[(hexString.Length) / 2];
+                try
+                {
+                    for (i = 0; i < returnBytes.Length; i++)
+                    {
+                        returnBytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("含有非16进制字符", "提示");
+                    return null;
+                }
+                return returnBytes;
+            }
+        }
+    }
+}
